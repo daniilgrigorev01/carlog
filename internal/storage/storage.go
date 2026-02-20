5d3c566af57c644a9600601a193dd21a28469230
Daniil Grigorev <daniilgrigorev01@mail.ru>
2026-02-20 12:39:11 +0500

    refactor(storage): rename the GetJournalByIId function
    Renamed the GetJournalByIId function to GetJournalByIID.
    
    This is done to keep the function names consistent.

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,7 +2,7 @@ package storage
 
 type Storage interface {
 	CreateJournal(name string, mileage int) (Journal, error)
-	GetJournalById(id int) (Journal, error)
+	GetJournalByID(id int) (Journal, error)
 	GetAllJournals() ([]Journal, error)
 	UpdateJournalName(id int, name string) (Journal, error)
 	UpdateJournalMileage(id int, mileage int) (Journal, error)
