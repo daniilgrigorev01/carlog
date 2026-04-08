d9901f6ccb29a8ba411e896f1815a2a61bae6e28
Daniil Grigorev <daniilgrigorev01@mail.ru>
2026-04-08 23:54:42 +0500

    feat(storage): add initial database migration script

---
+++ internal/storage/migrations/001_init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS journals (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE,
+  current_mileage INTEGER NOT NULL DEFAULT 0 CHECK (current_mileage >= 0 ),
+  mileage_last_update_at INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
+);
+
+CREATE TABLE IF NOT EXISTS records (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  journal_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  sku TEXT NOT NULL DEFAULT "",
+  replaced_at INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
+  next_date INTEGER DEFAULT NULL,
+  mileage INTEGER NOT NULL DEFAULT 0 CHECK(mileage >= 0),
+  next_mileage INTEGER DEFAULT NULL,
+  created_at INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
+  FOREIGN KEY (journal_id) REFERENCES journals(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS schema_migrations (
+  version INTEGER PRIMARY KEY AUTOINCREMENT,
+  applied_at INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
+)
