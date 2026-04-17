0948acfe97d06645e4b565319b641ee72b5cef80
Daniil Grigorev <daniilgrigorev01@mail.ru>
2026-04-17 22:23:35 +0500

    feat(storage): add contextual error helper for not found cases

--- internal/storage/errors.go
+++ internal/storage/errors.go
@@ -1,7 +1,14 @@
 package storage
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrNotFound = errors.New("not found")
 )
+
+func errNotFound(entity string, id int64) error {
+	return fmt.Errorf("storage: %s %d: %w", entity, id, ErrNotFound)
+}
